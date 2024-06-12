6d795db6a30450edaa9387040710b7851d8541b3
JghJaime <palomojr01@gmail.com>
2024-06-12 13:05:28 +0200

    así a ver

--- 01-ascii.js
+++ 01-ascii.js
@@ -2,7 +2,6 @@
 function randInt(max) {
     return Math.floor(Math.random() * (max + 1))
 }
-
 let numero = randInt(255);
 let caracter = String.fromCharCode(numero);
 console.log(caracter);  // Output: A
